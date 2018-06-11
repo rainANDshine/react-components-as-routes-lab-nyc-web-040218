2629237050896e23102ef3f70e479c49bd6ea315
github username <github email address>
2018-06-11 17:36:13 -0400

    Done.

--- src/components/Movies.js
+++ src/components/Movies.js
@@ -4,7 +4,16 @@ import { movies } from '../data';
 const Movies = () => {
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movies.map(movie => (
+        <div>
+          <h3>Name: {movie.title}</h3>
+          <p>Time: {movie.time}</p>
+          <ul>Genres:
+            {movie.genres.map(genre => <li>{genre}</li>)}
+          </ul>
+        </div>
+      ))}
     </div>
   );
 };
